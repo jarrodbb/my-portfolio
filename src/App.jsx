7c0715bda2e2a74c86c947435fb7e94aa5667c4c
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-10-06 11:23:47 +1000

    App.jsx set up. Nav and Outlet added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css';
-import BucketList from './components/BucketList';
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="bucket-app">
-      <BucketList />
-    </div>
+    <>
+      <Nav />
+      <Outlet />
+    </>
   );
 }
 
