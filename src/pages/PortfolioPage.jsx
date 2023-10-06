531a4417cddfb02b2ced00e9583a7b50269999a4
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-10-06 12:27:26 +1000

    projects added to PortfolioPage

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,3 +1,39 @@
+const project = [
+  {
+    id: 1,
+    name: "Sickie.com",
+    description: "Fullstack MVC application, utilising sequelize",
+    github: "https://github.com/jarrodbb/patient-zero",
+    deployedLink: "https://takesickies-40fabe44386a.herokuapp.com/",
+    image: "",
+  },
+  {
+    id: 2,
+    name: "Tech Blog",
+    description: "Fullstack MVC application, utilising sequelize",
+    github: "https://github.com/jarrodbb/Tech-Blog",
+    deployedLink: "https://understanding-tech-blog-eefe5bbb75c4.herokuapp.com/",
+    image: "",
+  },
+  {
+    id: 3,
+    name: "Text Editor",
+    description: "PWA with offline functionality",
+    github: "https://github.com/jarrodbb/Text-Editor",
+    deployedLink: "https://its-a-text-editor-6c1743480971.herokuapp.com/",
+    image: "",
+  },
+  {
+    id: 4,
+    name: "Note Taker",
+    description: "UPDATE",
+    github: "https://github.com/jarrodbb/Note-Taker",
+    deployedLink:
+      "https://organise-your-life-note-taker-5a8022c9039c.herokuapp.com/",
+    image: "",
+  },
+];
+
 export default function PortfolioPage() {
   return (
     <div>
