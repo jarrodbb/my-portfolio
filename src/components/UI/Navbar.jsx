6667acbf33541c4afc33b61b27d005d28bcaadd3
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-10-06 11:58:28 +1000

    name added to navbar

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,14 +1,14 @@
 export default function Nav({ links }) {
-    return (
-      <nav className="navbar navbar-expand-lg bg-secondary">
-        <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              {links.map((link) => link)}
-            </ul>
-          </div>
+  return (
+    <nav className="navbar navbar-expand-lg bg-secondary">
+      <div className="container-fluid">
+        <h1>Jarrod Blanning</h1>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {links.map((link) => link)}
+          </ul>
         </div>
-      </nav>
-    );
-  }
-  
+      </div>
+    </nav>
+  );
+}
