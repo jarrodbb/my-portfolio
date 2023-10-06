c6f1cc4c9908286c3aa01e2aa90b68c4986dc028
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-10-06 16:02:32 +1000

    contact form

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,112 @@
+import { useState } from "react";
+import "./style.css";
+
+import { validateEmail } from "../utils/helpers";
+
 export default function ContactPage() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // return inputType === 'name' ? setName(inputValue)
+
+    if (inputValue === "") {
+      setErrorMessage(inputType + " test");
+      return;
+    }
+
+    if (inputType === "email") {
+      setEmail(inputValue);
+    }
+    if (inputType === "name") {
+      setName(inputValue);
+    }
+
+    if (inputType === "message") {
+      setMessage(inputValue);
+    }
+
+    //   if (inputType === "email" ) {
+    //     if (inputValue === "") {
+    //       setErrorMessage("cannot be empty");
+    //     } else {
+    //       setEmail(inputValue);
+    //     }
+    //   }
+    //   if (inputType === "name") {
+    //     if (inputValue === "") {
+    //       setErrorMessage("cannot be empty");
+    //     } else {
+    //       setName(inputValue);
+    //     }
+    //   }
+    //   if (inputValue === "message") {
+    //     if (inputValue === "") {
+    //       setErrorMessage("cannot be empty")
+    //   } else {
+    //     setMessage(inputValue);
+    //   }
+    // }
+
+    // setErrorMessage("");
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorMessage("Email is invalid, please try again");
+      return;
+    }
+
+    alert(`Thanks for your email ${name}. I'll be intouch :)`);
+
+    setName("");
+    setMessage("");
+    setErrorMessage("");
+    setEmail("");
+  };
+
   return (
-    <div>
-      <h1>contact me</h1>
+    <div className="container text-center">
+      <form className="form" onSubmit={handleFormSubmit}>
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="your name"
+          required
+        />
+
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="message"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
