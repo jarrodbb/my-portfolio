50a6d6b090ca8c6ee4801d73f460526afab3e405
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-10-08 12:24:42 +1000

    reference updated. unused code in header removed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import jarrodB from "../assets/images/J_blanning.jpg"
+
 import "./Header.css";
 export default function Header({}) {
     return (
@@ -7,26 +7,3 @@ export default function Header({}) {
       </div>
     )
 }
-
-// <div className="image">
-// <img src = {jarrodB}/>
-// </div>
-// <div className="text">
-// <h1>Jarrod Blanning</h1>
-
-<div className="container">
-<div className="card">
-    <div className="row">
-        <div className="col-md-4">
-        <img className="img-fluid rounded-start"src = {jarrodB}/>
-        </div>
-<div className="col-md-8">
-<div className="card-body">
-<h3 className="card-title">Jarrod Blanning</h3>
-</div>
-
-</div>
-    </div>
-
-</div>
-</div>
